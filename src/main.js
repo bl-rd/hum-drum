880553c1e61ca059a6e4c199d5468f2b24579017
Bill Reid <william.c.reid@hotmail.co.uk>
2017-07-26 13:02:50 +0100

    Initial working instance of Tapage class

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-// `require` any node modules, `import` any custom modules (thanks rollup)
+import Sounder from './sounder';
+
+let s = new Tapage(120);
+const over = ['', 'd', '', 'd', '', 'g', '', 'g'];
+const under = ['d', 'd', 'd', 'd', 'g', 'g', 'g', 'g']
+s
+    .addTrack(over)
+    .addTrack(under)
+    .play();
---
+++ todo.md
@@ -0,0 +1,13 @@
+todo
+====
+
+## User journey
+
+- Create an instance of sounder
+  - give it a `bpm`
+  - give it a time signature (!)
+- Add a track
+  - this will be a list of notes
+- Add another track
+  - this will be added to the play list
+- Run the thing with a `play()` command
